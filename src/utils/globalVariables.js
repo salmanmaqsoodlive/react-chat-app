b0f9c04423dfd99590f14b883b3b85855884dffb
salmanmaqsoodlive <isalmanmaqsood@gmail.com>
2024-01-08 12:48:35 +0500

    added variables

--- src/utils/globalVariables.js
+++ src/utils/globalVariables.js
@@ -1,2 +1,2 @@
 export const BASE_URL = "http://ec2-34-228-70-78.compute-1.amazonaws.com:8000";
-export const IP = "ws:// 34.228.70.78:8000/ws/sc";
+export const IP = "ws://34.228.70.78:8000/ws/sc";
